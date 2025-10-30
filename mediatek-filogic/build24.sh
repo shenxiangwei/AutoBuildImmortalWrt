94ce39423d6c6cbede10b2d61465b0acf3fbe9ef
沈相玮 <30333034+shenxiangwei@users.noreply.github.com>
2025-10-30 12:03:45 +0800

    Update build24.sh

--- mediatek-filogic/build24.sh
+++ mediatek-filogic/build24.sh
@@ -55,8 +55,8 @@ PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-openvpn-zh-cn"
 # PACKAGES="$PACKAGES luci-i18n-appfilter-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
-PACKAGES="$PACKAGES luci-app-adblock"
-PACKAGES="$PACKAGES luci-app-airconnect"
+# PACKAGES="$PACKAGES luci-app-adblock"
+# PACKAGES="$PACKAGES luci-app-airconnect"
 
 #24.10.0
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
